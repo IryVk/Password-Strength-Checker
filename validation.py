4efc100d39934d5c6ab2b1169c92e04588059cd3
Arwa Essam Mesameh Abdelaziz <aa2101585@tkh.edu.eg>
2022-12-16 19:43:16 +0200

    Organized files

---
+++ validation.py
@@ -0,0 +1,41 @@
+SPECIAL = "!@#$%^&*()-+."
+UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+LOWER = "abcdefghijklmnopqrstuvwxyz"
+
+
+def main():
+    ...
+
+
+def check_length():
+    ...
+
+
+def check_upper():
+    ...
+
+
+def check_lower():
+    ...
+
+
+def check_digit():
+    ...
+
+
+def check_special():
+    ...
+
+
+# TO-DO
+def check_unique():
+    ...
+
+
+def check_common():
+    # Load list of common passwords in memory
+    ...
+
+
+if __name__ == "__main__":
+    main()
