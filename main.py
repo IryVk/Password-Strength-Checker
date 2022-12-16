331044e65cb318c552d09730898e65ec176a34ec
Arwa Essam Mesameh Abdelaziz <aa2101585@tkh.edu.eg>
2022-12-16 21:58:08 +0200

    joined and used all functions in main.py

--- main.py
+++ main.py
@@ -1,9 +1,91 @@
-import validation as v
+from validation import *
 
 
 def main():
-    ...
+    username = input("Username: ")
+    password = input("Password: ")
 
+    errors, index = calc_strength(username, password)
+
+    for error in errors:
+        print(error)
+    
+    if 0 <= index <= 5:
+        strength = "Very Weak"
+
+    elif 6 <= index <= 9:
+        strength = "Weak"
+
+    elif 10 <= index <= 12:
+        strength = "Moderate"
+
+    elif 13 <= index <= 14:
+        strength = "Strong"
+
+    else:
+        strength = "Very Strong"
+
+    print(f"Password is {strength}.")
+
+
+
+
+# Checks strength of password
+def calc_strength(usr, pswd):
+    errors = []  # Errors to guide users
+    str_index = 0  # Strength index to judge password
+    
+    # Length
+    if not check_length(pswd):
+        errors.append("Password is less than 8 characters.")
+        str_index += 0
+    else:
+        str_index += 3
+
+    # Uppercase
+    if not check_upper(pswd):
+        errors.append("Password does not contain any uppercase letters.")
+        str_index += 0
+    else:
+        str_index += 1
+
+    # Lowercase
+    if not check_lower(pswd):
+        errors.append("Password does not contain any lowercase letters.")
+        str_index += 0
+    else:
+        str_index += 1
+
+    # Numbers
+    if not check_digit(pswd):
+        errors.append("Password does not contain any numbers.")
+        str_index += 0
+    else:
+        str_index += 2
+
+    # Special Chars
+    if not check_special(pswd):
+        errors.append("Password does not contain any special characters from the following: !@#$%^&*()-+.")
+        str_index += 0
+    else:
+        str_index += 2
+
+    # Similar to username
+    if not check_unique(usr, pswd):
+        errors.append("Password should not contain sequences from your username.")
+        str_index += 0
+    else:
+        str_index += 2
+
+    # Common Password
+    if not check_common(pswd):
+        errors.append("Password contains commonly used phrases.")
+        str_index += 0
+    else:
+        str_index += 4
+
+    return errors, str_index
+    
 
 if __name__ == "__main__":
     main()
--- validation.py
+++ validation.py
@@ -4,64 +4,62 @@ LOWER = ("abcdefghijklmnopqrstuvwxyz")
 
 
 def main():
-    
-  ...  
+    ...  
 
+
+# Checks if password is at least 8 characters long
 def check_length(pswd):
     l = len(pswd)
     if l >= 8:
         return True
-
     return False
         
 
+# Checks if password contains a uppercase character
 def check_upper(pswd):
     for char in pswd:
         if char in UPPER:
             return True
-
     return False
     
 
+# Checks if password contains a lowercase character
 def check_lower(pswd):
     for char in pswd:
         if char in LOWER:
             return True
-
     return False
 
 
-def check_digit(password):
-    
-    for char in password:
+# Checks if password contains number
+def check_digit(pswd):
+    for char in pswd:
         if char.isdigit():
             return True
     return False
 
 
 
+# Checks if password contains special characters
 def check_special(password):
-    
-    for sp in password:
-        if sp in SPECIAL:
+    for char in password:
+        if char in SPECIAL:
             return True
     return False
-
     
 
-
-# TO-DO
+# Checks if password does not contain sequence in user-name
 def check_unique(usr, pswd):
     for i in range(len(pswd)):
         try:
-            if pswd[i:i+4] in usr:
+            if pswd[i:i+4].lower() in usr.lower():
                 return False
         except IndexError:
             break
-
     return True
 
 
+# Checks if passowrd is not in list of common passwords
 def check_common(pswd):
     # Load list of common passwords in memory
     common = []
@@ -71,13 +69,9 @@ def check_common(pswd):
     
     # Compare password with common passwords
     for password in common:
-        if pswd == password:
+        if pswd.lower() == password.lower():
             return False
-    
     return True
-
-
-
 
 
 if __name__ == "__main__":
